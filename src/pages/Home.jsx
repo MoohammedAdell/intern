cc86144ebdbd589e24d81387be29004b69b0cd75
mohamed adel <mohammedadell496@gmail.com>
2026-06-11 19:36:36 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+        <>
+          <header>
+            <nav>
+                
+            </nav>
+          </header>
+        </>
+    )
+}
+
+export default Home
